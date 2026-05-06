c4cc9a63856c0e8173b5fff4c70a444a78678717
Shreya-103 <shreyaalpha1@gmail.com>
2026-05-06 08:20:24 +0530

    component-based

--- components/card.jsx
+++ components/card.jsx
@@ -1,13 +1,16 @@
 const Cards = ()=>{
 
-return(
-<div key={idx} className="images">
+let printUserData = "Loading..."
+    if(userData.length>0){
+      printUserData = userData.map(function(elem, idx){
+        return <div key={idx} className="images">
           <a href={elem.url} target="_blank">
           <img src={elem.download_url} alt="elem.author" />
           <h5>{elem.author}</h5>
           </a>
         </div>
-)
+      });
+    }
 }
 
 export default Cards
