dc1b262e7fe677e76a123efc23e3c0b8c1225e22
Shreya-103 <shreyaalpha1@gmail.com>
2026-05-07 08:32:32 +0530

    corrected card error

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,11 @@
 const Cards = (props)=>{
+    return(
        <div>
           <a href={props.elem.url} target="_blank">
           <img src={props.elem.download_url} alt="elem.author" />
           <h5>{props.elem.author}</h5>
           </a>
       </div>
-      };
+      )};
 
 export default Cards
